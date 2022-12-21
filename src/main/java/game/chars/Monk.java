0235ea0c1368304225e6e963fe2d335a00112e88
Maria Lipina <m.o.lip714@gmail.com>
2022-12-21 21:47:20 +0300

    добавила к персонажам, Party, Fight новые перегрузки, чтобы создавать юнит-отряды как одного юнита

--- src/main/java/game/chars/Monk.java
+++ src/main/java/game/chars/Monk.java
@@ -18,6 +18,10 @@ public class Monk extends BaseHero {
         restoration = quantity;
     }
 
+    public Monk(int x, int y, String fraction, BattleField field, int quantity) {
+        super(12, 7, new int[]{-4,-4}, 30, 5, "Monk", x, y, fraction, field, quantity);
+    }
+
     public Monk(int attack, int protection, int[] damage, int health, int speed,
                 String name, int x, int y, String fraction, BattleField field) {
         super(attack, protection, damage, health, speed, name, x, y, fraction, field);
@@ -28,6 +32,11 @@ public class Monk extends BaseHero {
                 String name, int x, int y, String fraction, BattleField field, int quantity) {
         super(attack, protection, damage, health, speed, name, x, y, fraction, field, quantity);
         restoration = quantity;
+    }
+
+    public Monk(int attack, int protection, int[] damage, int health, int speed,
+                String name, int x, int y, String fraction, BattleField field, int quantity) {
+        super(attack, protection, damage, health, speed, name, x, y, fraction, field, quantity);
     }
 
     private float mostDamaged;
