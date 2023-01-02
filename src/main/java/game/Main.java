e58e3f9b99ae98cb8739034b4f552d7b67612fa3
Maria Lipina <m.o.lip714@gmail.com>
2023-01-02 21:32:36 +0300

    Добавила JavaDoc к Logger и больше TODO комментариев

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException { //TODO: да у меня тут исключения совсем не обработаны
 
         int step = 0;
         int teamSize = 10;
