88dbe0aac5feb067fa74558b98da9dde5d18fb98
Maria Lipina <m.o.lip714@gmail.com>
2023-01-04 11:15:10 +0300

    Добавила JavaDoc к BaseInterface и BaseHero

--- src/main/java/game/chars/BaseHero.java
+++ src/main/java/game/chars/BaseHero.java
@@ -4,6 +4,9 @@ import game.BattleField;
 
 import java.util.Arrays;
 
+/**
+ * Класс описывает персонажа, участвующего в сражении.
+ */
 public abstract class BaseHero implements BaseInterface {
     protected int attack;
     protected int defense;
@@ -28,6 +31,20 @@ public abstract class BaseHero implements BaseInterface {
     private static int idCount = 0;
     protected int id;
 
+
+    /**
+     * Создает персонажа, участвующего в сражении.
+     * @param attack Бонус к атаке может определить точность её попадания по цели и величину нанесенного урона.
+     * @param defense Бонус к защите может определить точность попадания со стороны противника и величину полученного урона.
+     * @param damage Минимальное и максимальное значение урона, который может нанести персонаж.
+     * @param health Очки здоровья.
+     * @param speed Скорость реакции на поле боя.
+     * @param name Имя персонажа.
+     * @param x Номер строки в матрице {@link game.BattleField BattleField}.
+     * @param y Номер столбца в матрице {@link game.BattleField BattleField}. Вместе с параметром x определяет положение персонажа на поле боя.
+     * @param fraction Название фракции, команды, относительно которой персонаж различает союзников и противников.
+     * @param field Ссылка на объект класса {@link game.BattleField BattleField}, который и \вляется полем боя с переданными на него координатами x и y.
+     */
     public BaseHero(int attack, int defense, int[] damage, int health, int speed,
                     String name, int x, int y, String fraction, BattleField field) {
         this.attack = attack;
@@ -47,6 +64,21 @@ public abstract class BaseHero implements BaseInterface {
         this.damageValue = 0;
     }
 
+
+    /**
+     * Создает персонажа как отряд, участвующий в сражении. Все персонажи в отряде действуют как один.
+     * @param attack Бонус к атаке может определить точность её попадания по цели и величину нанесенного урона.
+     * @param defense Бонус к защите может определить точность попадания со стороны противника и величину полученного урона.
+     * @param damage Минимальное и максимальное значение урона, который может нанести персонаж.
+     * @param health Очки здоровья.
+     * @param speed Скорость реакции на поле боя.
+     * @param name Имя персонажа.
+     * @param x Номер строки в матрице {@link game.BattleField BattleField}.
+     * @param y Номер столбца в матрице {@link game.BattleField BattleField}. Вместе с параметром x определяет положение персонажа на поле боя.
+     * @param fraction Название фракции, команды, относительно которой персонаж различает союзников и противников.
+     * @param field Ссылка на объект класса {@link game.BattleField BattleField}, который является полем боя с переданными на него координатами x и y.
+     * @param quantity Количество персонажей в одном отряде.
+     */
     public BaseHero(int attack, int defense, int[] damage, int health, int speed,
                     String name, int x, int y, String fraction, BattleField field, int quantity) {
         this.attack = attack;
@@ -68,35 +100,63 @@ public abstract class BaseHero implements BaseInterface {
         this.damageValue = 0;
     }
 
+
+    /**
+     * @return String название команды, к которой относится вызывающий объект {@link game.chars.BaseHero BaseHero}
+     */
     public String getFraction() {
         return fraction;
     }
 
+
+    /**
+     * @return String cтатус персонажа, который определяет его возможности в течение хода.
+     * <p>stand - жив и может совершить ход, быть целью других персонажей.</p>
+     * <p>used - для {@link game.chars.Peasant Peasant}, что нет боеприпасов для передачи персонажам-стрелкам {@link game.chars.Xbowman Xbowman}, {@link game.chars.Sniper Sniper}. Такой Peasant может совершать ход и являться целью других персонажей.</p>
+     * <p>used - для {@link game.chars.Xbowman Xbowman}, {@link game.chars.Sniper Sniper}, что у них боеприпас закончился. Такой персонаж может быть целью других, но не может совершить ход.</p>
+     * <p>dead - умер. Не совершает ход и не является целью других персонажей.</p>
+     */
     public String getStatus() {
         return status;
     }
 
-    /* Шпаргалка по имеющимся статусам
-     * stand - для всех. Жив и готов сражаться. По сути аналогичен статусу alive
-     * used - для крестьян, что они свою стрелу подали. Или для стрелков, что у них боеприпас закончился, а здоровье ещё нет
-     * dead - умер. Здоровье = или меньше 0
-     * */
 
+    /**
+     * @return координаты персонажа в форме объекта класса {@link game.chars.Coordinates Coordinates}.
+     */
     public Coordinates getPosition() {
         return position;
     }
 
+
+    /**
+     * @return имя персонажа, установленное при создании, в формате String.
+     */
     public String getName() {
         return name;
     }
 
+
+    /**
+     * @return очки здоровья в формате float.
+     */
     public float getHealth() {
         return health;
     }
 
+
+    /**
+     * Сделать ход в процессе игры.
+     * @param party объект класса {@link game.chars.Party Party}. Через него персонаж, совершающий ход, получает информацию о других участниках сражения: союзниках и противниках
+     */
     @Override
     public void step(Party party) {}
 
+
+    /**
+     * Количество очков здоровья изменяется на значение, переданное в параметре damage.
+     * @param damage целое число, обозначающее количество урона, который получает персонаж, вызывающие метод. Число может быть отрицательным, тогда персонаж лечится. очки здоровья Во втором случае невозможно, полечив, установить количество очков здоровья больше максимального, определенного при создании персонажа.
+     */
     protected void damage(int damage) {
         health = health - damage;
         if (health <= 0) {
@@ -105,6 +165,7 @@ public abstract class BaseHero implements BaseInterface {
         }
         if (health > maxHealth) health = maxHealth;
     }
+
 
     protected int damageValue(BaseHero h) {
         int flag = attack - h.defense;
@@ -115,6 +176,11 @@ public abstract class BaseHero implements BaseInterface {
         return value;
     }
 
+    /**
+     * Предоставляет базовую информацию о параметрах персонажа, которые могут непосредственно повлиять на совершение хода.
+     * Применяется в начале игры для первой записи в лог.
+     * @return строка String, в которой перечислены: название команды, имя, бонус к атаке, бонус к защите, минимальное и максимальное значение урона, текущие очки здоровья, скорость, статус, координаты на поле боя.
+     */
     @Override
     public String getInfo() {
         return "fraction=" + fraction +
@@ -129,6 +195,10 @@ public abstract class BaseHero implements BaseInterface {
 
     }
 
+    /**
+     * Вызывается каждый раунд (см. {@link game.Fight Fight}) для каждого члена в {@link game.chars.Party Party} Предоставляет данные о персонаже для записи в лог игрового процесса (см. {@link game.Logger Logger}).
+     * @return String содержит: название команды, имя вместе с id, координаты на поле боя, имя цели вместе с её id, координаты цели на поле боя, фактическая величина урона в данном ходу, статус цели.
+     */
     public String defaultLog() {
         try {
             return String.join(";", fraction, name + id,
